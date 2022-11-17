0b80557a8a75a6a66c070508f0f197aa3f1bd771
Ward Nakchbandi <hotrod.master@hotmail.com>
2022-11-17 17:16:49 +0000

    rebrand to pikaos

--- nobara-session/nobara-session/etc/profile.d/nobara-gnome-libunity.sh
+++
@@ -1,4 +0,0 @@
-# Apply libunity env vars to nobara session
-export XDG_CURRENT_DESKTOP="ubuntu:$XDG_CURRENT_DESKTOP" 
-export XDG_SESSION_DESKTOP="ubuntu:$XDG_SESSION_DESKTOP" 
-export DESKTOP_SESSION="ubuntu:$DESKTOP_SESSION"
