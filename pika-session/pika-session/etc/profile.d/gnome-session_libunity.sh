48a0ab9cfad6ba7208e7e67b23304bd61f986e73
Ward Nakchbandi <hotrod.master@hotmail.com>
2022-12-02 19:17:12 +0300

    partial sync

---
+++ pika-session/pika-session/etc/profile.d/gnome-session_libunity.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# Workaround X11 Desktop detection issue
+echo "$DESKTOP_SESSION" > /tmp/desktop-session
+# Apply libunity env vars to pika session
+if cat /tmp/desktop-session | grep pika > /dev/null 2>&1
+then
+        export XDG_CURRENT_DESKTOP="ubuntu:$XDG_CURRENT_DESKTOP" 
+        export XDG_SESSION_DESKTOP="ubuntu:$XDG_SESSION_DESKTOP" 
+        export DESKTOP_SESSION="ubuntu:$DESKTOP_SESSION"
+        rm /tmp/desktop-session 
+else 
+        rm /tmp/desktop-session 
+fi
+# Apply libunity env vars to vrr session
+if cat /tmp/desktop-session | grep vrr > /dev/null 2>&1
+then
+        export XDG_CURRENT_DESKTOP="ubuntu:$XDG_CURRENT_DESKTOP" 
+        export XDG_SESSION_DESKTOP="ubuntu:$XDG_SESSION_DESKTOP" 
+        export DESKTOP_SESSION="ubuntu:$DESKTOP_SESSION"
+        rm /tmp/desktop-session 
+else 
+        rm /tmp/desktop-session 
+fi
