2de80becfb1a703ed7d31c673e8e8b2e26f1f0b5
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-01-12 16:50:25 +0300

    pika-meta: Update ROCm meta & add apx to package manager, a few additions to codec meta

---
+++ pika-meta/pika-meta/debian/rocm-meta/etc/profile.d/rocm-meta.sh
@@ -0,0 +1,3 @@
+export ROC_ENABLE_PRE_VEGA=1
+export PATH=$PATH:/opt/rocm/bin:/opt/rocm/profiler/bin:/opt/rocm/opencl/bin:/opt/rocm/hip/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rocm/lib:/opt/rocm/lib64:/opt/rocm/profiler/lib:/opt/rocm/profiler/lib64:/opt/rocm/opencl/lib:/opt/rocm/hip/lib:/opt/rocm/opencl/lib64:/opt/rocm/hip/lib64
